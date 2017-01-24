13f8338f0703c0d6562885ad8373e909d55d68ff
Leonard Brünings <leonard.bruenings@1und1.de>
2017-01-24 16:01:04 +0100

    More

--- src/main/java/org/oneandone/tech/poker/leo/PokerController.java
+++ src/main/java/org/oneandone/tech/poker/leo/PokerController.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.view.RedirectView;
@@ -47,8 +48,8 @@ public class PokerController {
         return modelAndView;
     }
 
-    @RequestMapping(path = "/join/{gameId}", method = RequestMethod.GET)
-    public ModelAndView requestJoinGame(@PathVariable("gameId") String gameId,ModelAndView modelAndView) {
+    @RequestMapping(path = "/join", method = RequestMethod.GET)
+    public ModelAndView requestJoinGame(@RequestParam("gameId") String gameId,ModelAndView modelAndView) {
         GameSession game = getGame(gameId);
         modelAndView.addObject("gameId", game.getId());
         modelAndView.setViewName("join");
